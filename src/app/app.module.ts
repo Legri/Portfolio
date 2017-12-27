97607b13c10a4990fb04f39f18c28f5f672a17a8
Roman Kusmirchuk <Roman.Kusmirchuk@gmail.com>
2017-12-27 18:52:15 +0300

    Navbar and simple footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-
+import { MainmenuComponent } from './mainmenu/mainmenu.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuModule} from '@angular/material/menu';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainmenuComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatMenuModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/mainmenu/mainmenu.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {MatMenuModule} from '@angular/material/menu';
+
+@Component({
+  selector: 'app-mainmenu',
+  templateUrl: './mainmenu.component.html',
+  styleUrls: ['./mainmenu.component.css']
+})
+export class MainmenuComponent implements OnInit {
+  av_frelance="AVAILABLE FOR FREELANCE";
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
